6c87458b31f6e261ce029acd3d82b2c0644b5e1f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-03 17:09:02 +0900

    ゴルーチンの勉強をした

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-practice-goroutine-channel
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(value string) {
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(value)
+		time.Sleep(3 * time.Second)
+	}
+
+}
+
+func main() {
+
+	go f("goroutineを使って実行")
+
+	f("普通に実行")
+
+	fmt.Println("done")
+
+}
