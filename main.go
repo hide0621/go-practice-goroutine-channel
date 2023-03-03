49db5b164a6424237701ce66292cde2b6a934b6e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-03-03 17:27:02 +0900

    バッファとデッドロックの勉強をした

--- main.go
+++ main.go
@@ -16,20 +16,34 @@ func f(value string) {
 
 func main() {
 
+	// ゴルーチンの勉強
+
 	// go f("goroutineを使って実行")
 
 	// f("普通に実行")
 
 	// fmt.Println("done")
 
-	messages := make(chan string)
+	// 簡単なチャネルの勉強
 
-	go func() {
-		messages <- "Hello"
-	}()
+	// messages := make(chan string)
 
-	msg := <-messages
+	// go func() {
+	// 	messages <- "Hello"
+	// }()
 
-	fmt.Println(msg)
+	// msg := <-messages
 
+	// fmt.Println(msg)
+
+	// バッファとデッドロックの勉強
+
+	ch := make(chan int, 2)
+
+	// ch <- 1
+	// ch <- 2
+	// ch <- 3
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
