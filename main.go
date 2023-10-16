ad14f037763c23d15408794749e04b98c1da6cda
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-10-16 10:21:43 +0900

    sync.WaitGroupを使ってゴルーチンの終了を待ち受けられるようにした

--- main.go
+++ main.go
@@ -3,43 +3,35 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
-
-var st struct{ A, B, C int }
-
-var mutex *sync.Mutex
-
-func UpdateAndPrint(n int) {
-
-	mutex.Lock()
-
-	st.A = n
-	time.Sleep(time.Millisecond)
-	st.B = n
-	time.Sleep(time.Millisecond)
-	st.C = n
-	time.Sleep(time.Millisecond)
-	fmt.Println(st)
-
-	mutex.Unlock()
-}
 
 func main() {
 
-	mutex = new(sync.Mutex)
+	wg := new(sync.WaitGroup)
 
-	for i := 0; i < 5; i++ {
-		go func() {
-			for j := 0; j < 1000; j++ {
-				UpdateAndPrint(j)
-			}
-		}()
-	}
+	wg.Add(3)
 
-	// ゴルーチンが走る前にメインルーチンが終わらないようにしている
-	for {
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("1st Groutine")
+		}
+		wg.Done()
+	}()
 
-	}
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("2nd Groutine")
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("3rd Groutine")
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 
 }
